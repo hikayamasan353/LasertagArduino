aa3afdaf0208ec9878bc4418035e3b796393ff88
hikayamasan353 <oleg5538@yandex.ru>
2023-04-20 23:54:58 +0300

    Grenade update
    I guess it should be stable enough. Time to throw some bangabooms!!!

--- LasertagPrototype_Grenade/LasertagPrototype_Grenade.ino
+++ LasertagPrototype_Grenade/LasertagPrototype_Grenade.ino
@@ -15,8 +15,10 @@
 // Use 9V //
 ////////////
 
-int GREN_Emitter; //Common pin for all emitters. Connected to the ground.
-int GREN_Safety;  // Safety pin
+int GREN_Emitter=3; //Common pin for all emitters. Connected to the ground.
+//Should be PWM!!!
+int GREN_Safety=4;  // Safety pin
+bool detonated=false;//Has the grenade been detonated?
 
 
 void setup() {
@@ -34,17 +36,32 @@ void GREN_Detonate()
   MT2_Message(GREN_Emitter, SYS_MT2_Command, SYS_MT2_ExplodePlayer);
   //MT2_Message(GREN_Emitter, 0x83, 0x0b);
   // TODO: 0x83-0x0b
+  //TODO: BANG!!!! BOOOM!!! BAMMMM!!!!
   
+}
+
+//Reset the grenade by putting safety back in
+void GREN_Reset()
+{
+  detonated = false;
+  while (digitalRead(GREN_Safety) == HIGH) {
+    // Wait for safety pin to be pulled
+    delay(100);
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  if(GREN_Safety=LOW)
+  if(GREN_Safety==LOW&&!detonated)
   {
     delay(5000); //5 seconds
     GREN_Detonate();
+    detonated=true;
   }
+  //To reprime the grenade by putting the safety pin back
+  else if(detonated&&GREN_Safety==HIGH)
+      GREN_Reset();
   
 
 }
