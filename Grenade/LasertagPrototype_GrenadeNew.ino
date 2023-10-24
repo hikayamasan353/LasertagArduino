e81f6dce38db19a170c2857a7fec5b9ad5769b96
hikayamasan353 <oleg5538@yandex.ru>
2023-10-24 20:34:35 +0300

    Create LasertagPrototype_GrenadeNew.ino
    Grenade test code. Haven't tested it yet, use at your own risk.

---
+++ Grenade/LasertagPrototype_GrenadeNew.ino
@@ -0,0 +1,68 @@
+/////////////////////////////
+// KOZAC Grenade/Flashbang //
+/////////////////////////////
+#include "IRremote.hpp"
+
+
+////////////////////////////////////////////////////
+// Grenade has multiple LED emitters and a sound  //
+// speaker for the explosion imitation.           //
+// In flashbang mode, the emitter pin is used for //
+// LED flash that is strong enough to cause       //
+// floaters. USE WITH CAUTION AND SUPERVISION!!!  //
+////////////////////////////////////////////////////
+// POWER  //
+// Use 9V //
+////////////
+
+//Common pin for all emitters. Connected to the ground.
+IRsend GREN_Emitter(3);
+int GREN_Safety=4;  // Safety pin
+bool detonated=false;//Has the grenade been detonated?
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(GREN_Safety, INPUT);
+
+}
+
+//Fire the grenade
+void GREN_Detonate()
+{
+  //MilesTag 2 explode player
+  // [Header]-0x83-0x0B-0xE8
+  unsigned long packet=((0x83<<8)&0x0b)<<8&0xE8;
+  GREN_Emitter.sendSony(packet,24);
+
+  //MT2_Message(GREN_Emitter, 0x83, 0x0b);
+  // TODO: 0x83-0x0b
+  //TODO: BANG!!!! BOOOM!!! BAMMMM!!!!
+  
+}
+
+//Reset the grenade by putting safety back in
+void GREN_Reset()
+{
+  detonated = false;
+  while (digitalRead(GREN_Safety) == HIGH) {
+    // Wait for safety pin to be pulled
+    delay(100);
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  if(GREN_Safety==LOW&&!detonated)
+  {
+    delay(5000); //5 seconds
+    GREN_Detonate();
+    detonated=true;
+  }
+  //To reprime the grenade by putting the safety pin back
+  else if(detonated&&GREN_Safety==HIGH)
+      GREN_Reset();
+  
+
+}
